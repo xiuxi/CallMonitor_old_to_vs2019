494bd4cdf5e18320692a26cc49ab45e8771aeb29
xiuxi <xiuxi554325746@gmail.com>
2022-01-01 11:40:39 +0800

    a

---
+++ test_2019_not_ok/Test.cpp
@@ -0,0 +1,15 @@
+
+#include <windows.h>
+#include <iostream>
+#include "include.h"
+
+void test() {
+    //cout << "hello aaaa \n" << endl;
+}
+
+int main(int argc,char *argv[])
+{
+    test();
+	
+	return 0;
+}
